a220aa9fa023a7b01a2684ed8e370e44c067ff85
abu bakar <abubakar850772@gmail.com>
2024-12-14 16:35:13 +0600

    add getAllProducts controller for retrieved all products

--- src/app/modules/products/product.controller.ts
+++ src/app/modules/products/product.controller.ts
@@ -1,6 +1,11 @@
 import { Request, Response } from 'express';
-import { insertProduct, isProductExist } from './product.services';
+import {
+    findAllProducts,
+    insertProduct,
+    isProductExist,
+} from './product.services';
 import productValidationSchema from './product.validation';
+import { z } from 'zod';
 
 const createProduct = async (req: Request, res: Response) => {
     try {
@@ -11,13 +16,48 @@ const createProduct = async (req: Request, res: Response) => {
         const productAlreadyExist = await isProductExist(productData.name);
         if (productAlreadyExist) throw new Error('Product already exist');
 
-        const createdProduct = await insertProduct(productData)
+        const createdProduct = await insertProduct(productData);
         if (!createdProduct)
             throw new Error('Something went wrong while inserting product');
         res.status(201).json({
             message: 'Bike Created Successfully',
             success: true,
             data: productData,
+        });
+    } catch (error: unknown) {
+        if (error instanceof z.ZodError) {
+            res.status(500).json({
+                message: error.issues[0].message || 'Something went wrong',
+                success: false,
+                error: error,
+                stack: error.stack,
+            });
+        } else if (error instanceof Error) {
+            res.status(500).json({
+                message: error.message || 'Something went wrong',
+                success: false,
+                error: error,
+                stack: error.stack,
+            });
+        } else {
+            res.status(500).json({
+                message: 'Unknown error occurred',
+                success: false,
+                error: error,
+            });
+        }
+    }
+};
+
+const getAllProducts = async (req: Request, res: Response) => {
+    try {
+        const queryData = req.query;
+        const products = await findAllProducts(queryData);
+        if (!products) throw new Error('Product not found');
+        res.status(200).json({
+            message: 'Bikes retrieved successfully',
+            success: true,
+            data: products,
         });
     } catch (error: unknown) {
         if (error instanceof Error) {
@@ -37,4 +77,4 @@ const createProduct = async (req: Request, res: Response) => {
     }
 };
 
-export { createProduct };
+export { createProduct, getAllProducts };
