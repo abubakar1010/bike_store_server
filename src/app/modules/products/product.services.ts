572f0b411f3a7a80eb9ddce38a4748d3a9705086
abu bakar <abubakar850772@gmail.com>
2024-12-14 16:29:13 +0600

    add findAllProducts service

--- src/app/modules/products/product.services.ts
+++ src/app/modules/products/product.services.ts
@@ -1,4 +1,4 @@
-import { IProduct } from './product.interface';
+import { IProduct, IProductQuery } from './product.interface';
 import { Product } from './products.model';
 
 const isProductExist = async (name: string) => {
@@ -11,4 +11,9 @@ const insertProduct = async(productData: IProduct) => {
     return createdProduct
 }
 
-export { isProductExist,insertProduct };
+const findAllProducts = async(query: IProductQuery) => {
+    const products = await Product.find(query)
+    return products
+}
+
+export { isProductExist,insertProduct, findAllProducts };
