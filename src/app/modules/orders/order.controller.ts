747a5c8f496ff879d045ea2fb326c552088d02d0
abu bakar <abubakar850772@gmail.com>
2024-12-14 20:22:19 +0600

    add generateRevenue controller for calculating total revenue

--- src/app/modules/orders/order.controller.ts
+++ src/app/modules/orders/order.controller.ts
@@ -1,5 +1,10 @@
 import { Request, Response } from 'express';
-import { insertOrder, isOrderAlreadyExist, updateStock } from './order.service';
+import {
+    calculateRevenue,
+    insertOrder,
+    isOrderAlreadyExist,
+    updateStock,
+} from './order.service';
 import orderValidationSchema from './order.validation';
 import { z } from 'zod';
 import { findSpecificProduct } from '../products/product.services';
@@ -23,14 +28,17 @@ const createOrder = async (req: Request, res: Response) => {
                 `Insufficient stock: Only ${product.quantity} units are available.`,
             );
 
-        const orderAlreadyExist = await isOrderAlreadyExist(orderData.product, orderData.email);
+        const orderAlreadyExist = await isOrderAlreadyExist(
+            orderData.product,
+            orderData.email,
+        );
         if (orderAlreadyExist) throw new Error('Order already exist');
 
         const createdOrder = await insertOrder(orderData);
         if (!createdOrder)
             throw new Error('Something went wrong while inserting order');
 
-        await updateStock(orderData.product, orderData.quantity)
+        await updateStock(orderData.product, orderData.quantity);
 
         res.status(201).json({
             message: 'Order Created Successfully',
@@ -61,4 +69,34 @@ const createOrder = async (req: Request, res: Response) => {
         }
     }
 };
-export { createOrder };
+
+const generateRevenue = async (req: Request, res: Response) => {
+    try {
+        const revenue = await calculateRevenue();
+        if(revenue <= 0) throw new Error("Something went wrong while calculating revenue")
+        res.status(200).json({
+            message: 'Revenue calculated successfully',
+            status: true,
+            data: {
+                totalRevenue: revenue,
+            },
+        });
+    } catch (error: unknown) {
+        if (error instanceof Error) {
+            res.status(500).json({
+                message: error.message || 'Something went wrong',
+                success: false,
+                error: error,
+                stack: error.stack,
+            });
+        } else {
+            res.status(500).json({
+                message: 'Unknown error occurred',
+                success: false,
+                error: error,
+            });
+        }
+    }
+};
+export { createOrder, generateRevenue };
+
