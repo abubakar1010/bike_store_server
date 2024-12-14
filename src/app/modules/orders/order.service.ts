da0df7293dc5f9e9d7e3b713385d7392d64dca63
abu bakar <abubakar850772@gmail.com>
2024-12-14 20:20:08 +0600

    add calculateRevenue service

--- src/app/modules/orders/order.service.ts
+++ src/app/modules/orders/order.service.ts
@@ -9,16 +9,37 @@ const isOrderAlreadyExist = async (productId: string, email: string) => {
 
 const insertOrder = async (orderData: IOrder) => {
     await Order.create(orderData);
-    const orderedData = Order.findOne({ product: orderData.product, email: orderData.email });
+    const orderedData = Order.findOne({
+        product: orderData.product,
+        email: orderData.email,
+    });
     return orderedData;
 };
 
-const updateStock = async(productId: string, requestedQuantity: number) => {
-    const product = await Product.findByIdAndUpdate(productId,{$inc:{quantity: -requestedQuantity}}, {new: true});
-    if(product && product.quantity <= 0){
+const updateStock = async (productId: string, requestedQuantity: number) => {
+    const product = await Product.findByIdAndUpdate(
+        productId,
+        { $inc: { quantity: -requestedQuantity } },
+        { new: true },
+    );
+    if (product && product.quantity <= 0) {
         product.inStock = false;
-        product.save()
+        product.save();
     }
-}
+};
 
-export { isOrderAlreadyExist, insertOrder, updateStock };
+const calculateRevenue = async () => {
+    const totalRevenue = await Order.aggregate([
+        {
+            $group: {
+                _id: null,
+                totalRevenue: { $sum: '$totalPrice' },
+            },
+        },
+    ]);
+
+    const revenue = totalRevenue.length > 0 ? totalRevenue[0].totalRevenue : 0;
+    return revenue;
+};
+
+export { isOrderAlreadyExist, insertOrder, updateStock, calculateRevenue };
