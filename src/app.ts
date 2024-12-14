50b4ab43c08346a75482e98a333bd2614ab5f0ee
abu bakar <abubakar850772@gmail.com>
2024-12-14 16:30:12 +0600

    add productRoute middleware

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,17 @@
 import express, { Request, Response } from 'express'
+import cors from 'cors'
+import { productRoute } from './app/modules/products/product.route'
 export const app = express()
+
+// express middleware 
+
+app.use(cors())
+app.use(express.json())
+
+
+// routes
+
+app.use("/api",productRoute )
 
 app.get("/", (req: Request, res: Response) => {
     res.json({message: "SERVER CONNECTED"})
