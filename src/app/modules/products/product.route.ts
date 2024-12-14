addecc54c7c85a3a00e7d280f30e989e06461be8
abu bakar <abubakar850772@gmail.com>
2024-12-14 17:16:55 +0600

    add PUT /products/:productId endpoint for update existed product

--- src/app/modules/products/product.route.ts
+++ src/app/modules/products/product.route.ts
@@ -1,10 +1,11 @@
 import { Router } from "express";
-import { createProduct, getAllProducts, getSpecificProducts } from "./product.controller";
+import { createProduct, getAllProducts, getSpecificProducts, updateProduct } from "./product.controller";
 
 const router = Router()
 
 router.route("/products").post(createProduct)
 router.route("/products").get(getAllProducts)
 router.route("/products/:productId").get(getSpecificProducts)
+router.route("/products/:productId").put(updateProduct)
 
 export const productRoute = router
