1606f8faf9d21b9845861b276ba5f113b5ba90b9
abu bakar <abubakar850772@gmail.com>
2024-12-14 16:32:30 +0600

    add timestamp for track create and update time

--- src/app/modules/products/products.model.ts
+++ src/app/modules/products/products.model.ts
@@ -1,42 +1,41 @@
-import { model, Schema } from "mongoose";
-import { IProduct } from "./product.interface";
+import { model, Schema } from 'mongoose';
+import { IProduct } from './product.interface';
 
 const productSchema = new Schema<IProduct>(
     {
         name: {
-          type: String,
-          required: true,
+            type: String,
+            required: true,
         },
         brand: {
-          type: String,
-          required: true,
+            type: String,
+            required: true,
         },
         price: {
-          type: Number,
-          required: true,
+            type: Number,
+            required: true,
         },
         category: {
-          type: String,
-          enum: ['Mountain', 'Road', 'Hybrid', 'Electric'],
-          required: true,
+            type: String,
+            enum: ['Mountain', 'Road', 'Hybrid', 'Electric'],
+            required: true,
         },
         description: {
-          type: String,
-          required: true,
+            type: String,
+            required: true,
         },
         quantity: {
-          type: Number,
-          required: true,
+            type: Number,
+            required: true,
         },
         inStock: {
-          type: Boolean,
-          required: true,
-        }
-      },
-      {
-        timestamps: true
-      }
-)
+            type: Boolean,
+            required: true,
+        },
+    },
+    {
+        timestamps: true,
+    },
+);
 
-
-export const Product = model<IProduct>("Product", productSchema)
+export const Product = model<IProduct>('Product', productSchema);
