585bb220de79a567ce5455ecae645d6d587d10a1
abu bakar <abubakar850772@gmail.com>
2025-02-01 15:19:31 +0600

    feat: add auth service

---
+++ src/app/modules/auth/auth.service.ts
@@ -0,0 +1,119 @@
+import bcrypt from 'bcrypt';
+import httpStatus from 'http-status';
+import { JwtPayload } from 'jsonwebtoken';
+import config from '../../config';
+import { User } from '../user/user.model';
+import { TLoginUser } from './auth.interface';
+import { createToken } from './auth.utils';
+import ApiError from '../../utils/apiError';
+
+const loginUser = async (payload: TLoginUser) => {
+
+    // checking if the user is exist
+    const user = await User.findOne({ email: payload.email });
+
+    if (!user) {
+        throw new ApiError(httpStatus.NOT_FOUND, 'This user is not found !');
+    }
+    // checking if the user is already deleted
+
+    const isDeleted = user?.isDeleted;
+
+    if (isDeleted) {
+        throw new ApiError(httpStatus.FORBIDDEN, 'This user is deleted !');
+    }
+
+    // checking if the user is blocked
+
+    const userStatus = user?.status;
+
+    if (userStatus === 'deActive') {
+        throw new ApiError(
+            httpStatus.FORBIDDEN,
+            'This user account is deactivate ! !',
+        );
+    }
+
+    //checking if the password is correct
+
+    console.log(user)
+
+    if (!(await User.isPasswordMatched(payload?.password, user?.password)))
+        throw new ApiError(httpStatus.FORBIDDEN, 'Password do not matched');
+
+    //create token and sent to the  client
+
+    const jwtPayload = {
+        email: user.email,
+        role: user.role,
+    };
+
+    const accessToken = createToken(
+        jwtPayload,
+        config.jwt_access_secret as string,
+    );
+
+    return {
+        accessToken,
+    };
+};
+
+const changePassword = async (
+    userData: JwtPayload,
+    payload: { oldPassword: string; newPassword: string },
+) => {
+    // checking if the user is exist
+    const user = await User.findOne({ email: userData.email });
+
+    if (!user) {
+        throw new ApiError(httpStatus.NOT_FOUND, 'This user is not found !');
+    }
+    // checking if the user is already deleted
+
+    const isDeleted = user?.isDeleted;
+
+    if (isDeleted) {
+        throw new ApiError(httpStatus.FORBIDDEN, 'This user is deleted !');
+    }
+
+    // checking if the user is blocked
+
+    const userStatus = user?.status;
+
+    if (userStatus === 'deActive') {
+        throw new ApiError(
+            httpStatus.FORBIDDEN,
+            'This user account is deActive ! !',
+        );
+    }
+
+    //checking if the password is correct
+
+    if (!(await User.isPasswordMatched(payload.oldPassword, user?.password)))
+        throw new ApiError(httpStatus.FORBIDDEN, 'Password do not matched');
+
+    //hash new password
+    const newHashedPassword = await bcrypt.hash(
+        payload.newPassword,
+        Number(config.bcrypt_salt_rounds),
+    );
+
+    await User.findOneAndUpdate(
+        {
+            email: userData.email,
+            role: userData.role,
+        },
+        {
+            password: newHashedPassword,
+            needsPasswordChange: false,
+            passwordChangedAt: new Date(),
+        },
+    );
+
+    return null;
+};
+
+export const AuthServices = {
+    loginUser,
+    changePassword,
+};
