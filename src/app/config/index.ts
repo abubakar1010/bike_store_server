959f07da8b5d142406e8492327e6b2d618dc2867
abu bakar <abubakar850772@gmail.com>
2025-02-01 12:50:28 +0600

    feat: add auth utility to create and verify jwt token

--- src/app/config/index.ts
+++ src/app/config/index.ts
@@ -1,7 +1,18 @@
 import dotenv from 'dotenv';
 import path from 'path';
 
-dotenv.config({ path: path.join(process.cwd(), '.env') });
+dotenv.config({ path: path.join((process.cwd(), '.env')) });
 
-export const DB_URI = process.env.DB_URI as string;
-export const PORT = process.env.PORT || 5000;
+export default {
+    NODE_ENV: process.env.NODE_ENV,
+    port: process.env.PORT,
+    database_url: process.env.DATABASE_URL,
+    bcrypt_salt_rounds: process.env.BCRYPT_SALT_ROUNDS,
+    jwt_access_secret: process.env.JWT_ACCESS_SECRET,
+    jwt_refresh_secret: process.env.JWT_REFRESH_SECRET,
+    jwt_access_expires_in: process.env.JWT_ACCESS_EXPIRES_IN,
+    jwt_refresh_expires_in: process.env.JWT_REFRESH_EXPIRES_IN,
+    cloudinary_cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+    cloudinary_api_key: process.env.CLOUDINARY_API_KEY,
+    cloudinary_api_secret: process.env.CLOUDINARY_API_SECRET,
+};
