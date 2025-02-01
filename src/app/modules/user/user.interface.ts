1a9d70017ff5977474483c6e369f56a598c5a17b
abu bakar <abubakar850772@gmail.com>
2025-02-01 14:42:47 +0600

    feat: add user interface and model

---
+++ src/app/modules/user/user.interface.ts
@@ -0,0 +1,25 @@
+/* eslint-disable no-unused-vars */
+import { Model } from 'mongoose';
+import { USER_ROLE } from './constant';
+
+export interface TUser {
+    name: string;
+    email: string;
+    password: string;
+    profileImg?: string;
+    contactNo?: string;
+    address?: string;
+    status: 'active' | 'deActive';
+    role: 'admin' | 'user';
+    isDeleted: boolean;
+}
+
+export interface UserModel extends Model<TUser> {
+    //if passwords are matched
+    isPasswordMatched(
+        password: string,
+        hashedPassword: string,
+    ): Promise<boolean>;
+}
+
+export type TUserRole = keyof typeof USER_ROLE;
---
+++ src/app/modules/user/user.model.ts
@@ -0,0 +1,73 @@
+/* eslint-disable @typescript-eslint/no-this-alias */
+import bcrypt from 'bcrypt';
+import { Schema, model } from 'mongoose';
+import config from '../../config';
+import { userStatus } from './constant';
+import { TUser, UserModel } from './user.interface';
+const userSchema = new Schema<TUser, UserModel>(
+    {
+        name: {
+            type: String,
+            required: true,
+        },
+        email: {
+            type: String,
+            required: true,
+            unique: true,
+        },
+        password: {
+            type: String,
+            required: true,
+            select: 0,
+        },
+        profileImg: {
+            type: String,
+            default: '',
+        },
+        address: {
+            type: String,
+            default: '',
+        },
+        role: {
+            type: String,
+            enum: ['user', 'admin'],
+            default: 'user',
+        },
+        status: {
+            type: String,
+            enum: userStatus,
+            default: 'active',
+        },
+        isDeleted: {
+            type: Boolean,
+            default: false,
+        },
+    },
+    {
+        timestamps: true,
+    },
+);
+
+userSchema.pre('save', async function (next) {
+    const user = this;
+    // hash password
+    user.password = await bcrypt.hash(
+        user.password,
+        Number(config.bcrypt_salt_rounds),
+    );
+    next();
+});
+
+// set password = ''
+userSchema.post('save', function (doc, next) {
+    doc.password = '';
+    next();
+});
+userSchema.statics.isPasswordMatched = async function (
+    password,
+    hashedPassword,
+) {
+    return await bcrypt.compare(password, hashedPassword);
+};
+
+export const User = model<TUser, UserModel>('User', userSchema);
