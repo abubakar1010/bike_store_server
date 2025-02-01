4b736414bc8b6ffd74da31de5fe3e3e0c2dc1545
abu bakar <abubakar850772@gmail.com>
2025-02-01 15:12:20 +0600

    feat: add user route

---
+++ src/app/modules/user/user.route.ts
@@ -0,0 +1,33 @@
+import { Router } from 'express';
+import auth from '../../middlewares/auth';
+import { upload } from '../../utils/sendImageToCloudinary';
+import { USER_ROLE } from './constant';
+import { UserControllers } from './user.controller';
+
+const router = Router();
+
+router.post(
+    '/users/create-user',
+    upload.single('file'),
+    UserControllers.createUser,
+);
+
+router.get('/users', auth(USER_ROLE.admin), UserControllers.getAllUsers);
+
+router.get('/users/:id', UserControllers.getSingleUser);
+
+router.patch(
+    '/users/:id',
+    auth(USER_ROLE.admin, USER_ROLE.user),
+    UserControllers.updateUser,
+);
+
+router.delete('/users/:id', auth(USER_ROLE.admin), UserControllers.deleteUser);
+
+router.patch(
+    '/users/change-status/:id',
+    auth(USER_ROLE.admin),
+    UserControllers.changeStatus,
+);
+
+export const UserRoutes = router;
